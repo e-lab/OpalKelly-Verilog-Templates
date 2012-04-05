73627821fa51ede423181a8733a591d224989357
Berin Martini <berin.martini@gmail.com>
2012-04-05 13:56:29 -0400

    Remove temp files from software

--- software/xem3001v2-template/release/deps/okFrontPanelDLL.d
+++
@@ -1,3 +0,0 @@
-release/okFrontPanelDLL.o release/deps/okFrontPanelDLL.d : src/okFrontPanelDLL.cpp src/okFrontPanelDLL.h
-
-src/okFrontPanelDLL.h:
--- software/xem3001v2-template/release/deps/xem3001v2_template.d
+++
@@ -1,3 +0,0 @@
-release/xem3001v2_template.o release/deps/xem3001v2_template.d : src/xem3001v2_template.cpp src/okFrontPanelDLL.h
-
-src/okFrontPanelDLL.h:
